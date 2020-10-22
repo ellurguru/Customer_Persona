34ccbfc135d1b2c8beac7155b62862a52cbbd461
tri-g <62416318+tri-g@users.noreply.github.com>
2020-10-22 11:36:39 +0530

    Add files via upload

---
+++ src/app/prod_names.ts
@@ -0,0 +1,42 @@
+export var credit="Credit Card";
+
+export var credit_info=`<img class="img2" src="assets/images/prod_credit.jpg" style="float: right;
+margin-left:20px; border-radius: 8px;margin-bottom:4px;">
+<div>A credit card is a payment card issued to users to enable the cardholder to pay a merchant for goods and services based on the cardholder's promise to the card issuer to pay them for the amounts plus the other agreed charges.<br><b>Benefits:</b></div>
+<ul>
+<li>The main benefit to the cardholder is convenience.</li>
+<li>No interest is charged in the case when the full balance is paid within grace period.</li>
+<li>Many credit cards offer rewards and benefits packages.</li>
+</ul>`;
+
+export var loan="Loan";
+
+export var loan_info=`<img class="img2" src="assets/images/prod_loan.jpg" style="float: right;
+margin-left:20px; border-radius: 8px;margin-bottom:4px;">
+<div> A loan is the lending of money by one or more individuals, organizations, or other entities to other individuals, organizations etc.<br><b>Benefits:</b></div>
+<ul>
+<li>Low interest rates.</li>
+<li>Bank loans can provide you the flexibility to spend the money where you see fit.</li>
+<li>You don’t have to give up equity to get a loan from a bank.</li>
+</ul>`;
+
+export var saving="Savings Account";
+
+export var saving_info=`<img class="img2" src="assets/images/prod_saving.jpg" style="float: right;
+margin-left:20px; border-radius: 8px;margin-bottom:4px;">
+<div>A savings account is a bank account at a retail bank whose features include the requirements that only a limited number of withdrawals can take place, it does not have cheque facilities and usually do not have a linked debit card facility, it has limited transfer facilities and cannot be overdrawn.<br><b>Benefits:</b></div>
+<ul>
+<li>A Savings Account is a safe avenue for funds.</li>
+<li>Banks nowadays provide value added services and benefits with a Savings Account.</li>
+<li>A savings bank earns interest</li>
+</ul>`;
+export var current="Current Account";
+
+export var current_info=`<img class="img2" src="assets/images/prod_current.jpg" style="float: right;
+margin-left:20px; border-radius: 8px;margin-bottom:4px;">
+<div>Current bank account is opened by someone who have a higher number of regular transactions with the bank. It includes deposits, withdrawals, and contra transactions.<br><b>Benefits:</b></div>
+<ul>
+<li>It enables you to carry out your business transactions properly and promptly.</li>
+<li>You can withdraw from your current accounts without any limit.</li>
+<li>It enables the current account holder to obtain overdraft (short-term borrowing) facility.</li>
+</ul>`;
